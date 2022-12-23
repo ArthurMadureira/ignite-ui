da72ec7441868c284e80dca72fbef2daa5e7a0ae
Arthur Madureira <arthurmadureira10@gmail.com>
2022-12-23 18:59:16 -0300

    Add core components

---
+++ packages/docs/src/stories/Text.stories.tsx
@@ -0,0 +1,43 @@
+import type { Meta, StoryObj } from '@storybook/react'
+import { Text, TextProps } from '@ignite-ui/react'
+
+export default {
+  title: 'Typography/Text',
+  component: Text,
+  args: {
+    size: 'md',
+    children:
+      ' Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium doloribus, officiis iure, molestias dolorum aliquid laborum at voluptas alias mollitia iste aperiam laudantium pariatur magnam tempore nulla quo, omnis maxime.',
+  },
+  argTypes: {
+    size: {
+      options: [
+        'xxs',
+        'xs',
+        'sm',
+        'md',
+        'lg',
+        'xl',
+        '2xl',
+        '4xl',
+        '5xl',
+        '6xl',
+        '7xl',
+        '8xl',
+        '9xl',
+      ],
+      control: {
+        type: 'inline-radio',
+      },
+    },
+  },
+} as Meta<TextProps>
+
+export const Primary: StoryObj<TextProps> = {}
+
+export const CustomTag: StoryObj<TextProps> = {
+  args: {
+    children: 'Strong Text',
+    as: 'strong',
+  },
+}
